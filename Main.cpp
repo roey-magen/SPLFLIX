181a3638c24c6b9db6d60d0332c702907e154d94
roey-magen <57194211+roey-magen@users.noreply.github.com>
2019-11-21 20:33:04 +0200

    print roey in main

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include "../include/Session.h"
+#include "Session.h"
 using namespace std;
 
 int main(int argc, char** argv){
-	if(argc!=2)
-	{
-		cout << "usage splflix input_file" << endl;
-		return 0;
-	}
-	Session s(argv[1]);
-	s.start();
+//	if(argc!=2)
+//	{
+//		cout << "usage splflix input_file" << endl;
+//		return 0;
+//	}
+//	Session s(argv[1]);
+//	s.start();
+cout<<"reoy";
 	return 0;
 }
