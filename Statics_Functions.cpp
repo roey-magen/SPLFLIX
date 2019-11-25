59df3932a6d234f0579f926d0ca83915f4aa1a8e
tzach420 <54484969+tzach420@users.noreply.github.com>
2019-11-25 02:54:56 +0200

    added json reader function, need to test

--- Statics_Functions.cpp
+++ Statics_Functions.cpp
@@ -1,10 +1,16 @@
 //
 // Created by tzachu@wincs.cs.bgu.ac.il on 22/11/2019.
 //
-
+#include <iostream>
+#include <json.hpp>
+#include <fstream>
 #include "Statics_Functions.h"
+#include "Watchable.h"
 using namespace std;
-
+using json = nlohmann::json;
+class Watchable;
+class Movie;
+class Episode;
 
 string Statics_Functions::vector_to_string(vector<string> & vec) {
     string Tags="[";
@@ -13,4 +19,45 @@ string Statics_Functions::vector_to_string(vector<string> & vec) {
     Tags=Tags.substr(0, Tags.size()-1);
     Tags.at(Tags.size()-1)=']';
     return Tags;
+}
+void Statics_Functions::buildDataBase(const string& path, vector<Watchable*> & content) {
+    ifstream i("/home/tzach/CLionProjects/projectA/untitled/config1.json");///change to path later.
+    if(!i){///need to check what to do if failed to load file.(maybe remove at the end)
+        std::cout <<"Failed to open Json file. Please check the file path.";
+    }
+    json j;
+    i>>j;
+    ///finish reading file here
+    long index = 0;
+    for (auto it:j["movies"]) {
+        string movie_name = it["name"].get<std::string>();
+        int movie_length = it["length"];
+        vector<string> tags;
+        for (auto tag:it["tags"])
+            tags.push_back(tag.get<std::string>());
+        Watchable *movie = new Movie(index, movie_name, movie_length, tags);//check if we need to free this.
+        index++;
+        content.push_back(movie);
+    }
+    for (auto it:j["tv_series"]) {
+        string series_name = it["name"].get<std::string>();
+        int series_length = it["episode_length"];
+        vector<string> tags;
+        for (auto tag:it["tags"])
+            tags.push_back(tag.get<std::string>());
+        int season_number = 0;
+        for (auto season:it["seasons"]) {
+            season_number++;
+            int episode_number = 1;
+            while (season.get<std::int8_t>() != 0) {//need to check it reads!
+                Watchable *episode = new Episode(index, series_name, series_length, season_number, episode_number,
+                                                 tags);//check if we need to free.
+                content.push_back(episode);
+                index++;
+                episode_number++;
+            }
+
+        }
+
+    }
 }
--- Statics_Functions.h
+++ Statics_Functions.h
@@ -4,13 +4,18 @@
 
 #ifndef SPLFLIX_STATICS_FUNCTIONS_H
 #define SPLFLIX_STATICS_FUNCTIONS_H
+#include <iostream>
 #include <string>
 #include <vector>
+#include <json.hpp>
+#include "Watchable.h"
+using json = nlohmann::json;
 using namespace std;
 
 class Statics_Functions{
 public:
     static string vector_to_string(vector<string> &vec);
+    static void buildDataBase(const string& path, vector<Watchable*> & content);
 
 
 };
